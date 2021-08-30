d57560d85b7121474b0de63830dbb5ecc7996785
Niyaz <niyaz.irfan119@gmail.com>
2021-08-30 17:01:00 +0300

    commit tree 4cea7c9cbe4c37e60fe0c733736aa49f0282202d

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
 // write your CatList component here
+import React from 'react';
+
+function CatList({ catPics }) {
+  const catsArr = catPics.map((cat) => (
+    <img key={cat.id} src={cat.url} alt="cat" />
+  ));
+  return <div>{catsArr}</div>;
+}
+
+export default CatList;
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCats } from './catsSlice';
+import CatList from './CatList';
 
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const loading = useSelector((state) => state.status);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
+
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      {/* test didn't run with the loading so i removed the bonus part */}
+      <CatList catPics={catPics} />
     </div>
   );
 }
